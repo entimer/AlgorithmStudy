7eb6024052fc7cfa6aef2ecb999e7d42145e8272
entimer <cheonjaewoong@gmail.com>
2020-08-31 21:16:51 +0900

    2020-08-30 ~ 2020-08-31 문제풀이

--- study202008/d20200830_q2_미래도시.py
+++ study202008/d20200830_q2_미래도시.py
@@ -3,13 +3,40 @@
 ==============================================================================
 문제 위치: 교재 259p
 ==============================================================================
-소요 시간: 00분
+소요 시간: 30분
 """
+INFINITY = 100_000
 
-print("소스코드 작성")
+n, m = map(int, input().split())
+
+connect_list = [[INFINITY] * (n + 1) for _ in range(n + 1)]
+for x in range(1, n +1):
+    for y in range(1, n + 1):
+        if x == y:
+            connect_list[x][y] == 0
+
+for _ in range(m):
+    comp1, comp2 = map(int, input().split())
+    connect_list[comp1][comp2] = 1
+    connect_list[comp2][comp1] = 1
+
+x, k = map(int, input().split())
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        for l in range(1, n + 1):
+            connect_list[j][l] = min(connect_list[j][l], connect_list[j][i] + connect_list[i][l])
+
+distance = connect_list[1][k] + connect_list[k][x]
+
+if distance >= INFINITY:
+    print(-1)
+else:
+    print(distance)
 
 """
-접근법: 내가 왜 이런 방식으로 문제를 해결했는지 작성
+접근법: 책에 있는 플로이드 워셜 알고리즘을 그대로 쓰다 보니 소스가 거의 같다.
 ==============================================================================
-개선점: 개선할 수 있는 부분
+개선점: 반복문용 변수 i, j, l이 너무 비슷하게 생겨 오류 고치는데 시간을 많이 사용했다.
+반복변수는 이렇게 쓰지 않는 것이 좋겠다.
 """
--- study202008/d20200831_q1_전보.py
+++ study202008/d20200831_q1_전보.py
@@ -3,13 +3,51 @@
 ==============================================================================
 문제 위치: 교재 262p
 ==============================================================================
-소요 시간: 00분
+소요 시간: 못품
 """
+import heapq
 
-print("소스코드 작성")
+infinity = 100_000_000_000
+
+n, m, c = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+distance = [infinity] * (n + 1)
+for i in range(m):
+    x, y, z = map(int, input().split())
+    graph[x].append([y, z])
+
+def find(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+        
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost,  i[0]))
+
+find(c)
+
+count = 0
+max_distance = 0
+
+for d in distance:
+    if d != infinity:
+        count += 1
+        max_distance = max(max_distance, d)
+
+print(count - 1, max_distance)
 
 """
-접근법: 내가 왜 이런 방식으로 문제를 해결했는지 작성
+접근법: 해결하지 못해 책의 소스를 보고 씀
 ==============================================================================
-개선점: 개선할 수 있는 부분
+개선점: 다익스트라 알고리즘을 써야 한다는 것 까진 생각했지만 실제 구현에서 다양한 문제로 인해 오류를 겪음
+다익스트라 구현을 좀더 공부해보는 것이 좋을 듯 함.
 """
